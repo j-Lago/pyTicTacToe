399acec0999a7b16ebd9c0e79afae88e5f17ac2e
j-lago <jackson.lago@ifsc.edu.br>
2025-06-17 18:59:45 -0300

    initial

--- eval_move.py
+++ eval_move.py
@@ -1,12 +1,25 @@
-def eval_move(board: list[list[int]], r, c) -> tuple[list[list[int]], int]:
-    bsum = board_sum(board)
-    if board[r][c] == 0:
-        board[r][c] = 1 if bsum == 0 else -1
-    result = h_check(board) + v_check(board) + d_check(board)
+from outcome_convention import Outcome
 
-    if result == 0 and full_check(board):
-        result = 2
-    return board, result
+BoardSateType = list[list[int]]
+
+
+def eval_move(board: BoardSateType, next_move: tuple[int, int]) -> tuple[BoardSateType, Outcome]:
+
+    if not validate(board):
+        return board, Outcome.INVALID_STATE
+
+    r, c = next_move
+    if board[r][c] == 0:
+        board[r][c] = 1 if board_sum(board) == 0 else -1
+    else:
+        return board, Outcome.INVALID_MOVE
+
+    if (outcome := h_check(board)) != Outcome.CONTINUE: return board, outcome
+    if (outcome := v_check(board)) != Outcome.CONTINUE: return board, outcome
+    if (outcome := d_check(board)) != Outcome.CONTINUE: return board, outcome
+    if full_check(board): return board, Outcome.DRAW
+
+    return board, Outcome.CONTINUE
 
 
 def board_sum(board):
@@ -20,28 +33,28 @@ def validate(board):
     return board_sum(board) <= 1
 
 
-def h_check(board) -> int:
+def h_check(board) -> Outcome:
     for r in range(3):
         cum = 0
         for c in range(3):
             cum += board[r][c]
         if cum == 3:
-            return 1
+            return Outcome.X_WINS
         if cum == -3:
-            return -1
-    return 0
+            return Outcome.O_WINS
+    return Outcome.CONTINUE
 
 
-def v_check(board) -> int:
+def v_check(board) -> Outcome:
     for c in range(3):
         cum = 0
         for r in range(3):
             cum += board[r][c]
         if cum == 3:
-            return 1
+            return Outcome.X_WINS
         if cum == -3:
-            return -1
-    return 0
+            return Outcome.O_WINS
+    return Outcome.CONTINUE
 
 
 def full_check(board) -> bool:
@@ -52,11 +65,11 @@ def full_check(board) -> bool:
     return True
 
 
-def d_check(board) -> int:
+def d_check(board) -> Outcome:
     cum1 = board[0][0] + board[1][1] + board[2][2]
     cum2 = board[2][0] + board[1][1] + board[0][2]
     if cum1 == 3 or cum2 == 3:
-        return 1
+        return Outcome.X_WINS
     if cum1 == -3 or cum2 == -3:
-        return -1
-    return 0
+        return Outcome.O_WINS
+    return Outcome.CONTINUE
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from pathlib import Path
 import tkinter as tk
 from eval_move import eval_move
+from outcome_convention import Outcome
+
+
+
 
 class TicTacToe:
     def __init__(self):
         self.window = tk.Tk()
-        path = Path(__file__).parent / 'assets'
-        self.assets = { 'empty': tk.PhotoImage(file=path/'empty.png'),
-                        'o': tk.PhotoImage(file=path/'o.png'),
-                        'x': tk.PhotoImage(file=path/'x.png')}
+        self.window.title('Tic-Tac-Toe')
+        self.window.resizable(False, False)
 
+        path = Path(__file__).parent / 'assets'
+        self.assets = {
+            'empty': tk.PhotoImage(file=path/'empty.png'),
+            'o': tk.PhotoImage(file=path/'o.png'),
+            'x': tk.PhotoImage(file=path/'x.png')
+        }
         self.board = None
         self.buttons = None
         self.reset()
-
-        self.window.title('Tic-Tac-Toe')
-        self.window.resizable(False, False)
         self.window.mainloop()
 
     def reset(self):
@@ -23,24 +28,27 @@ class TicTacToe:
         self.create_board()
 
     def popup_message(self, message):
-        self.window.update_idletasks()  # Garante que as dimensões estejam atualizadas
-
         w, h = 320, 120
+        self.window.update_idletasks()
         x = (self.window.winfo_x()) + (self.window.winfo_width() // 2) - (w // 2) + 6
         y = (self.window.winfo_y()) + (self.window.winfo_height() // 2) - (h // 2) + 6
 
         popup = tk.Toplevel()
         popup.overrideredirect(True)
         popup.geometry(f"{w}x{h}+{x}+{y}")
-        bg2 = '#7F7F7F'
-        bg3 = '#777777'
-        fg = '#F1F1F1'
+        bg2, bg3, fg = '#7F7F7F','#777777', '#F1F1F1'
         font = 'Comic Sans MS'
         popup.config(bg=bg2)
         label = tk.Label(popup, text=message, font=(font, 22), bg=bg2, foreground=fg)
         label.pack(pady=10)
-        tk.Button(popup, text="RESET", font=(font, 16), command=popup.destroy, bg=bg3, activebackground=bg3, foreground=fg, relief='flat').pack()
+        tk.Button(popup, font=(font, 16), text="RESET",
+                  bg=bg3, activebackground=bg3, foreground=fg, relief='flat',
+                  command=lambda p=popup: self.popup_reset(p)).pack()
         popup.grab_set()
+
+    def popup_reset(self, popup):
+        popup.destroy()
+        self.reset()
 
     def create_board(self):
         self.buttons = [[None for _ in range(3)] for _ in range(3)]
@@ -54,32 +62,26 @@ class TicTacToe:
                                                    command=lambda r=row, c=col: self.make_move(r, c))
                 self.buttons[row][col].grid(row=row, column=col)
 
-
     def redraw(self):
         for r in range(3):
             for c in range(3):
                 match self.board[r][c]:
                     case 0: self.buttons[r][c].config(image=self.assets['empty'])
-                    case 1: self.buttons[r][c].config(image=self.assets['o'])
-                    case -1: self.buttons[r][c].config(image=self.assets['x'])
+                    case 1: self.buttons[r][c].config(image=self.assets['x'])
+                    case -1: self.buttons[r][c].config(image=self.assets['o'])
                     case _: raise ValueError(f"Os estados válidos do tabuleiro são 0, 1 e -1. Foi encontrado '{self.board[r][c]}' na posição [{r}][{c}].")
 
-
-
     def make_move(self, r, c):
-        self.board, result = eval_move(self.board, r, c)
+        self.board, outcome = eval_move(self.board, (r, c))
         self.redraw()
-        match result:
-            case 1:
-                self.popup_message("Jogador 'o' ganhou!")
-                self.reset()
-            case -1:
-                self.popup_message("Jogador 'x' ganhou!")
-                self.reset()
-            case 2:
-                self.popup_message("Empate!")
-                self.reset()
-            case _: pass
+        match outcome:
+            case Outcome.X_WINS: self.popup_message("Jogador 'x' ganhou!")
+            case Outcome.O_WINS: self.popup_message("Jogador 'o' ganhou!")
+            case Outcome.DRAW: self.popup_message("Empate!")
+            case Outcome.INVALID_MOVE: self.window.bell()
+            case Outcome.INVALID_STATE: raise ValueError(f"Tabuleiro inválido inválido: {self.board}")
+            case Outcome.CONTINUE: pass
+            case _: assert False
 
 
 
